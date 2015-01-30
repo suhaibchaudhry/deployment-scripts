bd24e6ac8aa2b13ec1d693c38c14b5bcd77cb73c
Suhaib <suhaib@uitoux.com>
2015-01-29 18:29:30 -0600

    Add create_server.sh

---
+++ create_server.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+echo "Setting variables"
+echo "What name do you want for your account? "
+read name
+name="$name"
+echo "What username do you want to create? "
+read user
+username="$user"
+echo "What sitename do you want to create? "
+read site
+sitename="$site"
+echo "What password do you want for root? "
+read rootpassword
+rootpass="$rootpassword"
+echo "What password do you want for your username? "
+read usernamepassword
+userpass="$usernamepassword"
+
+echo "Changing password for root"
+echo -e "$rootpass\n$rootpass" | passwd root
+
+echo "Creating a new user"
+useradd -s /bin/bash -m -d /home/"$username" -c "$name" -g "$username" "$username"
+echo -e "$userpass\n$userpass" | passwd "$username"
+
+echo "Installing LEMP stack"
+apt-add-repository ppa:nginx/stable -y
+apt-get install nginx -y
+apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+echo "deb http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
+echo "deb-src http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
+apt-get update -y
+apt-get dist-upgrade -y
+sudo debconf-set-selections <<< 'percona-server-server-5.5 percona-server-server-5.5/$rootpass password $rootpass'
+sudo debconf-set-selections <<< 'percona-server-server-5.5 percona-server-server-5.5/$rootpass password $rootpass'
+apt-get install percona-server-server-5.5 percona-server-client-5.5 -y
+apt-get install php5-fpm -y
+debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password'
+debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
+debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password $rootpass'
+debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password'
+debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect lighttpd'
+apt-get install phpmyadmin -y
+apt-get install drush -y
+apt-get install php5-cli -y
+apt-get install php5-mcrypt -y
+apt-get install php5-curl -y
+php5enmod mcrypt
+
+echo "Create filesystem"
+sudo "$username"
+cd ~
+mkdir "$sitename"
+cd "$sitename"
+wget http://ftp.drupal.org/files/projects/drupal-7.34.tar.gz
+tar -xvzf drupal-7.34.tar.gz
+rm drupal-7.34.tar.gz
+mv drupal-7.34 httpdocs
+sed -i 's/www-data/"$username"/g' /etc/nginx/nginx.conf
+cd /etc/nginx/sites-enabled/
+rm default
+rsync -azPK -e "ssh -p 2222" suhaib@96.88.40.226:/home/suhaib/backups/nginx_conf/sample_se.com /etc/nginx/sites-enabled/
+mv sample_se.com "$sitename".com
+sed -i 's/suhaib/$username/g' /etc/nginx/sites-enabled/"$sitename".com
+sed -i 's/uitoux/$sitename/g' /etc/nginx/sites-enabled/"$sitename".com
+sed -i 's/www-data/$username/g' /etc/php5/fpm/pool.d/www.conf
+sed -i 's/www-data/$username/g' /etc/php5/fpm/php-fpm.conf
+cd ~/"$sitename"/httpdocs
+ln -s /usr/share/phpmyadmin .
+cd sites
+mv default "$sitename".com
+ln -s "$sitename".com default
+exit
+
+echo "Restart all services"
+service nginx restart
+service php5-fpm restart
+
+echo "Installing ZSH and Oh-My-Zsh Packages"
+apt-get install zsh -y
+apt-get install git-core -y
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s 'which zsh'
+
+echo "Changing username shell to ZSH"
+sudo "$username"
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s 'which zsh'
+exit
+reboot
