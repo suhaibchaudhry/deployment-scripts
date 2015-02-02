8a2997b9e7a6219785d20df0f5c980d2654dfb92
Suhaib <suhaib@uitoux.com>
2015-02-02 15:21:09 -0600

    Output STDERR to log

--- create_server.sh
+++ create_server.sh
@@ -32,33 +32,33 @@ echo "$username":"$userpass" | /usr/sbin/chpasswd
 echo -e "\n\n"
 
 echo -e "adding nginx repo... \c"
-apt-add-repository ppa:nginx/stable -y > /dev/null 2>&1
+apt-add-repository ppa:nginx/stable -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "installing nginx... \c"
-apt-get install nginx -y > /dev/null 2>&1
+apt-get install nginx -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "installing key for percona... \c"
-apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A > /dev/null 2>&1
+apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A  > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "adding repo to sources list... \c"
 echo "deb http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
 echo "deb-src http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
 echo -e "done!"
 echo -e "updating package list... \c"
-apt-get update -y > /dev/null 2>&1
+apt-get update -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "upgrading distro... \c"
-apt-get dist-upgrade -y > /dev/null 2>&1
+apt-get dist-upgrade -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "setting preconfigured inputs for percona... \c"
 echo "percona-server-server-5.5 percona-server-server/root_password password $rootpass" | debconf-set-selections
 echo "percona-server-server-5.5 percona-server-server/root_password_again password $rootpass" | debconf-set-selections
 echo -e "done!"
 echo -e "installing percona... \c"
-apt-get install percona-server-server-5.5 percona-server-client-5.5 -y > /dev/null 2>&1
+apt-get install percona-server-server-5.5 percona-server-client-5.5 -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "installing php5... \c"
-apt-get install php5-fpm -y > /dev/null 2>&1
+apt-get install php5-fpm -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "setting preconfigured inputs for phpmyadmin... \c"
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
@@ -68,25 +68,25 @@ echo "phpmyadmin phpmyadmin/mysql/app-pass password $rootpass" | debconf-set-sel
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect lighttpd" | debconf-set-selections
 echo -e "done!"
 echo -e "installing phpmyadmin... \c"
-apt-get install phpmyadmin -y > /dev/null 2>&1
+apt-get install phpmyadmin -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "installing drush... \c"
-apt-get install drush -y > /dev/null 2>&1
+apt-get install drush -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "installing php5 cli... \c"
-apt-get install php5-cli -y > /dev/null 2>&1
+apt-get install php5-cli -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "installing php5 mcrypt... \c"
-apt-get install php5-mcrypt -y > /dev/null 2>&1
+apt-get install php5-mcrypt -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "enabling mcrypt... \c"
-php5enmod mcrypt > /dev/null 2>&1
+php5enmod mcrypt > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "installing php5 curl... \c"
-apt-get install php5-curl -y > /dev/null 2>&1
+apt-get install php5-curl -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "installing php5 gd... \c"
-apt-get install php5-gd -y > /dev/null 2>&1
+apt-get install php5-gd -y > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 
 echo -e "\n\n"
@@ -98,10 +98,10 @@ echo -e "owning "$sitename".com directory... \c"
 chown -R "$username":"$useranme" /home/"$username"/"$sitename".com
 echo -e "done!"
 echo -e "retrieving drupal... \c"
-wget http://ftp.drupal.org/files/projects/drupal-7.34.tar.gz -P /home/"$username"/"$sitename".com/ > /dev/null 2>&1
+wget http://ftp.drupal.org/files/projects/drupal-7.34.tar.gz -P /home/"$username"/"$sitename".com/ > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "extracing drupal... \c"
-tar -xvzf /home/"$username"/"$sitename".com/drupal-7.34.tar.gz -C /home/"$username"/"$sitename".com/ > /dev/null 2>&1
+tar -xvzf /home/"$username"/"$sitename".com/drupal-7.34.tar.gz -C /home/"$username"/"$sitename".com/ > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "removing drupal tar file... \c"
 rm /home/"$username"/"$sitename".com/drupal-7.34.tar.gz
@@ -145,10 +145,10 @@ echo -e "done!"
 echo -e "\n\n"
 
 echo -e "restarting nginx... \c"
-service nginx restart > /dev/null 2>&1
+service nginx restart > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 echo -e "restarting php5... \c"
-service php5-fpm restart > /dev/null 2>&1
+service php5-fpm restart > /dev/null 2> /home/"$username"/errors.log
 echo -e "done!"
 
 echo -e "\n\n"
