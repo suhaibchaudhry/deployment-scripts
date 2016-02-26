8ac20b888e21fe7622a6afea793532779097d4cc
Suhaib <suhaib@uitoux.com>
2016-02-26 13:05:12 -0600

    testing

--- test.sh
+++ test.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#echo -e "who are you"
-#read -s who
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-#source /home/suhaib/bitbucket/deployment_scripts/test2.sh
-echo -e "is this a new server or a recycled server? (y/n) \c"
-read servertype
-
-
-case $servertype in
-  'y'|'Y')
-    echo -e "yes"
-    ;;
-  'n'|'N')
-    echo -e "no"
-    ;;
-  *)
-    echo "what"
-    ;;
-esac
+echo $DIR
